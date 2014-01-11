1cacdb6159d7f61ab27c8479c31312c0af036aa9
X-Stranger <xstranger@gmail.com>
2014-01-11 18:42:14 +0300

    Trying to build dynamic sized layout. Problem getting real internal layout size

--- jbrickshooter/src/main/java/by/jbrickshooter/MainActivity.java
+++ jbrickshooter/src/main/java/by/jbrickshooter/MainActivity.java
@@ -1,15 +1,19 @@
 package by.jbrickshooter;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.FrameLayout;
+import android.widget.GridLayout;
+import android.widget.ImageView;
 
 public class MainActivity extends Activity {
 
@@ -57,7 +61,68 @@ public class MainActivity extends Activity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            GridLayout rootView = (GridLayout) inflater.inflate(R.layout.fragment_main, container, false);
+            Context context = rootView.getContext();
+
+            Point point = new Point();
+            this.getActivity().getWindowManager().getDefaultDisplay().getSize(point);
+            int width = point.x;
+            int height = point.y;
+            System.out.println("HEIGHT: " + height);
+            System.out.println("WIDTH: " + width);
+            int size = height > width ? width : height;
+            size /= 16;
+
+            GridLayout.Spec cornerLeft = GridLayout.spec(0, 3);
+            GridLayout.Spec cornerRight = GridLayout.spec(13, 3);
+            GridLayout.Spec spanTop = GridLayout.spec(0, 3);
+            GridLayout.Spec spanBottom = GridLayout.spec(13, 3);
+
+            ImageView item = new ImageView(context);
+            item.setBackgroundColor(Color.RED);
+            GridLayout.LayoutParams params = new GridLayout.LayoutParams(cornerLeft, spanTop);
+            params.width = size * 3;
+            params.height = size * 3;
+            item.setLayoutParams(params);
+            rootView.addView(item);
+
+            for (int j = 0; j < 3; j++) {
+                for (int i = 3; i < 13; i++) {
+                    item = new ImageView(context);
+                    item.setBackgroundColor(Color.rgb((i + j) * 5, (i + j) * 10, (i + j) * 15));
+                    params = new GridLayout.LayoutParams(GridLayout.spec(i), GridLayout.spec(j));
+                    params.width = size;
+                    params.height = size;
+                    item.setLayoutParams(params);
+                    rootView.addView(item);
+                }
+            }
+
+            item = new ImageView(context);
+            item.setBackgroundColor(Color.GREEN);
+            params = new GridLayout.LayoutParams(cornerRight, spanTop);
+            params.width = size * 3;
+            params.height = size * 3;
+            item.setLayoutParams(params);
+            rootView.addView(item);
+
+            item = new ImageView(context);
+            item.setBackgroundColor(Color.YELLOW);
+            params = new GridLayout.LayoutParams(cornerLeft, spanBottom);
+            params.width = size * 3;
+            params.height = size * 3;
+            item.setLayoutParams(params);
+            rootView.addView(item);
+
+            item = new ImageView(context);
+            item.setBackgroundColor(Color.BLUE);
+            params = new GridLayout.LayoutParams(cornerRight, spanBottom);
+            params.width = size * 3;
+            params.height = size * 3;
+            item.setLayoutParams(params);
+            rootView.addView(item);
+
             return rootView;
         }
     }
